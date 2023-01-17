780f111657146ffa60482d4f4b9b602659990df2
Nikolay <nikolay_play2000@mail.ru>
2023-01-17 13:27:26 +0300

    add footer

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -3,12 +3,15 @@
     <default-bar />
 
     <default-view />
+
+    <Footer />
   </v-app>
 </template>
 
 <script lang="ts" setup>
   import DefaultBar from './AppBar.vue'
   import DefaultView from './View.vue'
+  import Footer from './Footer.vue'
 import { useAppStore } from '@/store/app'
  const store = useAppStore()
 </script>
---
+++ src/layouts/default/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+
+<v-footer
+    class="bg-indigo-lighten-1 text-center d-flex flex-column justify-space-between"
+  >
+    <div>
+      <v-btn
+        v-for="icon in icons"
+        :key="icon"
+        class="mx-4"
+        :icon="icon"
+        variant="text"
+      ></v-btn>
+    </div>
+
+
+ <div class="text-h6">
+  Охранная организация Cокол
+ </div>
+    <div>
+      {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+    </div>
+  </v-footer>
+</template>
+
+<script lang="ts" setup>
+  //
+const icons =  [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ]
+</script>
