1599a20ab5b22d519afcd1904889200273aefd13
Nikolay <nikolay_play2000@mail.ru>
2023-01-17 13:42:04 +0300

    refactor footer

--- src/layouts/default/Footer.vue
+++ src/layouts/default/Footer.vue
@@ -3,24 +3,44 @@
 <v-footer
     class="bg-indigo-lighten-1 text-center d-flex flex-column justify-space-between"
   >
-    <div>
+  <div class="d-flex">
       <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        variant="text"
+        class="mx-2"
+        rounded="xl"
+      >
+        {{ link }}
+      </v-btn>
+</div>
+
+     <div>
+<v-btn
         v-for="icon in icons"
         :key="icon"
         class="mx-4"
         :icon="icon"
         variant="text"
       ></v-btn>
-    </div>
+     </div>
+         <div class="text-h6">
+    Охранная организация Cокол
+   </div>
 
 
- <div class="text-h6">
-  Охранная организация Cокол
- </div>
+
     <div>
       {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
     </div>
+
+
+
+
   </v-footer>
+
+
 </template>
 
 <script lang="ts" setup>
@@ -30,5 +50,10 @@ const icons =  [
         'mdi-twitter',
         'mdi-linkedin',
         'mdi-instagram',
+      ]
+const links =   [
+        'Услуги',
+        'Контакты',
+
       ]
 </script>
