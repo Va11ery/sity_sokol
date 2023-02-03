7ed22e777b9ab68d05454c70e0e9b7f19564aa31
Va11ery <109084396+Va11ery@users.noreply.github.com>
2023-02-03 07:52:13 +0000

    Deploying to gh-pages from @ Va11ery/sity_sokol@4e77072eead3300e4322073c1812cdb63adde7b6 🚀

---
+++ assets/VContainer.17cd9646.js
@@ -0,0 +1,1 @@
+import{m as t,u as n}from"./tag.7049cf31.js";import{m as r,u as s}from"./index.026f0829.js";const m=r({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...t()},setup(e,a){let{slots:o}=a;return n(()=>s(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},o)),{}}});export{m as V};
