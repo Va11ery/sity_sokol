4d21df6a67e6e1f0668cad8b9156debd35ccf8cc
Nikolay <nikolay_play2000@mail.ru>
2023-02-22 00:33:18 +0300

    fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
-    path: '/sity_sokol/',
+    path: '',
     component: () => import('@/layouts/default/Default.vue'),
     children: [
       {
@@ -40,7 +40,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/sity_sokol/'),
   routes,
 
 
