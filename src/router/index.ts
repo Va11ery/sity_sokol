0b84a7001cfca43616ba443661e844b2e82d515a
Nikolay <nikolay_play2000@mail.ru>
2023-03-09 22:12:27 +0300

    add

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,48 +1,52 @@
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 const routes = [
   {
-    path: '/sity_sokol/',
-    component: () => import('@/layouts/default/Default.vue'),
+    path: "/sity_sokol/",
+    component: () => import("@/layouts/default/Default.vue"),
     children: [
       {
-        path: 'home',
-        name: 'home',
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+        path: "",
+        name: "home",
+        component: () =>
+          import(/* webpackChunkName: "home" */ "@/views/Home.vue"),
       },
       {
-        path: 'about',
-        name: 'about',
-        component: () => import(/* webpackChunkName: "home" */ '@/views/AboutCompany.vue'),
+        path: "about",
+        name: "about",
+        component: () =>
+          import(/* webpackChunkName: "home" */ "@/views/AboutCompany.vue"),
       },
       {
-        path: 'service',
-        name: 'service',
-        component: () => import(/* webpackChunkName: "home" */ '@/views/ServiceCompany.vue'),
+        path: "service",
+        name: "service",
+        component: () =>
+          import(/* webpackChunkName: "home" */ "@/views/ServiceCompany.vue"),
       },
       {
-        path: 'clients',
-        name: 'clients',
-        component: () => import(/* webpackChunkName: "home" */ '@/views/ClientsCompany.vue'),
+        path: "clients",
+        name: "clients",
+        component: () =>
+          import(/* webpackChunkName: "home" */ "@/views/ClientsCompany.vue"),
       },
       {
-        path: 'teams',
-        name: 'teams',
-        component: () => import(/* webpackChunkName: "home" */ '@/views/TeamsCompany.vue'),
+        path: "teams",
+        name: "teams",
+        component: () =>
+          import(/* webpackChunkName: "home" */ "@/views/TeamsCompany.vue"),
       },
       {
-        path: 'contacts',
-        name: 'contacts',
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Contacts.vue'),
+        path: "contacts",
+        name: "contacts",
+        component: () =>
+          import(/* webpackChunkName: "home" */ "@/views/Contacts.vue"),
       },
     ],
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-
-
-})
-export default router
+});
+export default router;
